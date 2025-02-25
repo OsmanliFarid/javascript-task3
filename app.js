dbd04627da6ef964b24183a5973824f342ae76da
OsmanliFarid <osmanliferid686@gmail.com>
2025-02-25 20:56:26 +0400

    javascript change

--- app.js
+++ app.js
@@ -5,7 +5,17 @@ if(day === "bazar ertesi" || day === "Bazar ertesi") {
     mesaj = "bugun is gunudu"
 }else if(day === "cersenbe axsami" || day == "Cersenbe axsami"){
     mesaj = "bugun is gunudu"
-}else {
+}else if(day === "cersenbe" || day == "Cersenbe"){
+    mesaj = "bugun is gunudu"
+}else if(day === "cume axsami" || day == "Cume axsami"){
+    mesaj = "bugun is gunudu"
+}else if(day === "cume" || day == "Cume"){
+    mesaj = "bugun is gunudu"
+}else if(day === "senbe" || day == "Senbe"){
     mesaj = "bugun is gunu deyil"
+}else if(day === "bazar" || day == "Bazar"){
+    mesaj = "bugun is gunu deyil"
+}else {
+    mesaj = "lutfen doru gunu yazin"
 }
 console.log(mesaj)
